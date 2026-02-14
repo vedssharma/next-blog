29a40e6f65412199fab22e0eebf96a552bbe8cef
Ved Sharma <vedssharma@gmail.com>
2026-02-14 17:25:42 -0600

    update intro paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         {`Hey guys! I am Ved. I am a developer currently looking for a job.
-        In my freetime, I like to code, lift weights, and read books. Welcome to my blog/portfolio site.
+        In my freetime, I like to code, lift weights, play video games, and read books. Welcome to my blog/portfolio site.
         It will have all the stuff I have worked on (blog posts and side projects)`}
       </p>
       <div className="my-8">
