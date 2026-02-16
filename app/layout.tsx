eea513571b3fb28cfa0f4afbe408d7dc2e50bc3e
Ved Sharma <vedssharma@gmail.com>
2026-02-15 21:44:51 -0600

    update app's title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
+    default: "Ved Sharma's Portfolio",
     template: '%s | Next.js Portfolio Starter',
   },
   description: 'This is my portfolio.',
